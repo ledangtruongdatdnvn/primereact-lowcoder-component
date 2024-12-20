95da7a6615dacdad6b24e8c03bfeb3b063c16122
vietdanh1899 <nguyenvietdanhnph@gmail.com>
2024-12-20 11:14:37 +0700

    close others dropdown overlay

--- src/components/core/Dropdown/index.tsx
+++ src/components/core/Dropdown/index.tsx
@@ -19,7 +19,13 @@ import LabelWrapper from '../../../components/common/LabelWrapper';
 import { useDeepCompareEffect, useDeepCompareMemo } from 'use-deep-compare';
 import _ from 'lodash';
 import { IconType } from 'primereact/utils';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+
+declare global {
+  interface Window {
+    currentPrimeOverlay: Dropdown;
+  }
+}
 
 const defStaticProps = {
   placeholder: 'Select a City',
@@ -53,96 +59,104 @@ const FilterClearIcon: IconType<DropdownProps> = (options) => (
   </span>
 );
 
-let DropdownCompBase = (function () {
-  const childrenMap = {
-    staticProps: jsonControl(toJSONObject, defStaticProps),
-    defaultValue: jsonObjectExposingStateControl('defaultValue', defValue),
-    value: jsonObjectExposingStateControl('value', defValue),
-    label: stringExposingStateControl('label', ''),
-    error: stringExposingStateControl('error', ''),
-    caption: stringExposingStateControl('caption', ''),
-    showCaption: booleanExposingStateControl('showCaption', false),
-    required: booleanExposingStateControl('required', false),
-    options: jsonControl(toJSONObjectArray, defOptions),
-    onEvent: eventHandlerControl([
-      {
-        label: 'onChange',
-        value: 'change',
-        description: 'Triggers when Dropdown is selected.',
-      },
-    ]),
+const childrenMap = {
+  staticProps: jsonControl(toJSONObject, defStaticProps),
+  defaultValue: jsonObjectExposingStateControl('defaultValue', defValue),
+  value: jsonObjectExposingStateControl('value', defValue),
+  label: stringExposingStateControl('label', ''),
+  error: stringExposingStateControl('error', ''),
+  caption: stringExposingStateControl('caption', ''),
+  showCaption: booleanExposingStateControl('showCaption', false),
+  required: booleanExposingStateControl('required', false),
+  options: jsonControl(toJSONObjectArray, defOptions),
+  onEvent: eventHandlerControl([
+    {
+      label: 'onChange',
+      value: 'change',
+      description: 'Triggers when Dropdown is selected.',
+    },
+  ]),
+};
+
+const DropdownView = (props: any) => {
+  const handleChange = (e: any) => {
+    props.value.onChange(e.target.value);
+    props.onEvent('change');
   };
 
-  return new UICompBuilder(childrenMap, (props: any) => {
-    const handleChange = (e: any) => {
-      props.value.onChange(e.target.value);
-      props.onEvent('change');
-    };
+  useDeepCompareEffect(() => {
+    props.value.onChange?.(props.defaultValue?.value);
+  }, [props.defaultValue?.value]);
 
-    useDeepCompareEffect(() => {
-      props.value.onChange?.(props.defaultValue?.value);
-    }, [props.defaultValue?.value]);
+  const options = useDeepCompareMemo(() => props.options, [props.options]);
+  const staticProps = useDeepCompareMemo(() => props.staticProps, [props.staticProps]);
 
-    const options = useDeepCompareMemo(() => props.options, [props.options]);
-    const staticProps = useDeepCompareMemo(() => props.staticProps, [props.staticProps]);
+  const [filter, setFilter] = useState<string>('');
+  const dropdownRef = useRef<Dropdown>(null);
 
-    const [filter, setFilter] = useState<string>('');
+  return (
+    <LabelWrapper
+      label={props.label.value}
+      required={props.required.value}
+      error={props.error.value}
+      caption={props.caption.value}
+      showCaption={props.showCaption.value}
+    >
+      <Dropdown
+        ref={dropdownRef}
+        onShow={() => {
+          if (!dropdownRef.current) return;
+          if (dropdownRef.current?.getElement() !== window.currentPrimeOverlay?.getElement())
+            window.currentPrimeOverlay?.hide();
+          window.currentPrimeOverlay = dropdownRef.current;
+        }}
+        {..._.merge(staticProps, { style: { width: '100%' } })}
+        value={props.value.value}
+        options={options}
+        onChange={handleChange}
+        invalid={props.error.value.length > 0}
+        filterIcon={filter.length > 0 ? <></> : 'pi pi-search'}
+        filterClearIcon={FilterClearIcon}
+        onFilter={(e) => {
+          setFilter(e.filter);
+        }}
+      ></Dropdown>
+    </LabelWrapper>
+  );
+};
 
+const DropdownCompBase = new UICompBuilder(childrenMap, DropdownView)
+  .setPropertyViewFn((children: any) => {
     return (
-      <LabelWrapper
-        label={props.label.value}
-        required={props.required.value}
-        error={props.error.value}
-        caption={props.caption.value}
-        showCaption={props.showCaption.value}
-      >
-        <Dropdown
-          {..._.merge(staticProps, { style: { width: '100%' } })}
-          value={props.value.value}
-          options={options}
-          onChange={handleChange}
-          invalid={props.error.value.length > 0}
-          filterIcon={filter.length > 0 ? <></> : 'pi pi-search'}
-          filterClearIcon={FilterClearIcon}
-          onFilter={(e) => {
-            setFilter(e.filter);
-          }}
-        ></Dropdown>
-      </LabelWrapper>
+      <>
+        <Section name="Basic">
+          {children.staticProps.propertyView({ label: 'Static Props' })}
+          {children.defaultValue.propertyView({ label: 'Default Value' })}
+          {children.options.propertyView({ label: 'Options' })}
+        </Section>
+        <Section name="Interaction">{hiddenPropertyView(children)}</Section>
+        <Section name="Form">
+          {children.label.propertyView({ label: 'Label' })}
+          {children.error.propertyView({ label: 'Error' })}
+          {children.caption.propertyView({ label: 'Caption' })}
+          {children.showCaption.propertyView({ label: 'Show caption' })}
+          {children.required.propertyView({ label: 'Required' })}
+        </Section>
+        <Section name="Event">{children.onEvent.getPropertyView()}</Section>
+        <Section name="Description">
+          <ol className="text-sm p-0 m-0 mx-3">
+            <li>Static Props area should contain only static props of Primereact Dropdown.</li>
+            <li>
+              <a href="https://primereact.org/dropdown/" target="_blank">
+                More information
+              </a>
+            </li>
+          </ol>
+        </Section>
+      </>
     );
   })
-    .setPropertyViewFn((children: any) => {
-      return (
-        <>
-          <Section name="Basic">
-            {children.staticProps.propertyView({ label: 'Static Props' })}
-            {children.defaultValue.propertyView({ label: 'Default Value' })}
-            {children.options.propertyView({ label: 'Options' })}
-          </Section>
-          <Section name="Interaction">{hiddenPropertyView(children)}</Section>
-          <Section name="Form">
-            {children.label.propertyView({ label: 'Label' })}
-            {children.error.propertyView({ label: 'Error' })}
-            {children.caption.propertyView({ label: 'Caption' })}
-            {children.showCaption.propertyView({ label: 'Show caption' })}
-            {children.required.propertyView({ label: 'Required' })}
-          </Section>
-          <Section name="Event">{children.onEvent.getPropertyView()}</Section>
-          <Section name="Description">
-            <ol className="text-sm p-0 m-0 mx-3">
-              <li>Static Props area should contain only static props of Primereact Dropdown.</li>
-              <li>
-                <a href="https://primereact.org/dropdown/" target="_blank">
-                  More information
-                </a>
-              </li>
-            </ol>
-          </Section>
-        </>
-      );
-    })
-    .build();
-})();
+  .build();
 
 const exposingConfigs = [
   // new NameConfig('staticProps'),
